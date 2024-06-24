2c289f9df4917d1baa539c878c05646979624fa2
kriyanshi kamani <kriyanshikamani@gmail.com>
2024-06-24 17:37:44 +0530

    making Navbar

---
+++ User_panel/index.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- https://precision-brass-parts.com/resources/ -->
+</head>
+<body>
+   <nav class="navbar navbar-expand-lg navbar-light bg-danger ">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ">
+      <li class="nav-item active">
+        <a class="nav-link text-white font-bold  " href="#">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white font-bold" href="#">About Us</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white font-bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Product
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link disabled text-white font-bold" href="#">Quality</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link disabled text-white font-bold" href="#">Contact Us</a>
+      </li>
+    </ul>
+   
+  </div>
+</nav>
+</body>
+</html>
